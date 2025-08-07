355c51758e15eb55f0b403210f053a405c01d311
João Eduardo Araújo de Meneses <jeduardo.meneses@gmail.com>
2025-08-07 17:54:19 -0300

    feat: form state variables, form validation and route handling

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,8 +1,62 @@
 import Link from "next/link";
 import FormRegister from "../components/FormRegister";
+import { redirect } from "next/navigation";
 
 
 export default function Register() {
+  const handleRegister = async (_: string, formData: FormData) => {
+    "use server";
+
+
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+
+    if(!username || !email || !password){
+      return "Fill all fields!";      
+    }
+
+    if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
+      return "Invalid email!";
+    }
+
+    if(password.length < 6){
+      return "Password must be at least 6 characters long!";
+    }
+
+
+    try{
+      const body = {
+        username,
+        email,
+        password,
+      };
+
+      const res = await fetch("http://localhost:3000/auth/register", {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json",
+        }
+      });
+
+      const {token, message} = await res.json();
+
+      if(!token){
+        return message;      
+      }else{
+        // redirect("/tasks");
+      }
+  
+
+    }catch(e){
+      console.error("User register error : ", e);
+
+      return "User register error!";
+    }    
+    
+  }
 
 
   return (
@@ -10,7 +64,7 @@ export default function Register() {
       
       <h1 className="text-4xl text-center font-bold">Register</h1>
 
-      <FormRegister />
+      <FormRegister action={handleRegister}/>
 
       <Link className="text-center underline" href="/login">I'm already registered</Link>
 
