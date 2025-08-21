dfc3561d232aa8efca7cac3b4eff7c31191208b3
João Eduardo Araújo de Meneses <jeduardo.meneses@gmail.com>
2025-08-21 15:57:42 -0300

    feat: create task

--- src/app/(routes)/tasks/page.tsx
+++ src/app/(routes)/tasks/page.tsx
@@ -1,4 +1,8 @@
+import { FormTasks } from "@/app/components/FormTasks";
+import { fetchWithToken } from "@/lib/fetchWithToken";
 import { Metadata } from "next";
+import { cookies } from "next/headers";
+import { title } from "process";
 
 const PAGE_TITLE = "Tasks";
 
@@ -6,12 +10,66 @@ export const metadata: Metadata = {
   title: PAGE_TITLE
 };
 
-export default function Tasks() {    
+export default function Tasks() {   
+  
+  const handleCreateTask = async (_: string, formData: FormData) => {
+      "use server";
+  
+  
+      const task = formData.get("task")?.toString();
+  
+  
+      // if( !task){
+      //   return "You need inform the title of the task!";      
+  
+      // }
+
+      try{
+        const body = {
+          title: task
+        };
+
+      const cookieStore = await cookies();
+              
+      const token = cookieStore.get("token")?.value;
+
+      console.log("tokenTASKS: ", token);
+
+      if(!token){
+        return "Token not found!";
+      }else{
+        
+        const {message} = await fetchWithToken(
+          `${process.env.BACKEND_URL}/tasks`,
+          token,
+          {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {
+              "Content-Type": "application/json",
+          }
+        });
+        
+      
+        return message;
+        }
+      }catch(e){
+        console.error("handleCreateTask error : ", e);
+  
+        return "Error creating task!";
+      }    
+  
+      
+  
+    }
+
 
   return (
     <>
       
       <h1 className="text-4xl text-center font-bold">{PAGE_TITLE}</h1>
+
+      <FormTasks action={handleCreateTask}/>
 
       
     </>
---
+++ src/app/components/FormTasks.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { FC, useActionState, useState } from "react";
+import { FormInput } from "./FormInput";
+import { FormButton } from "./FormButton";
+import { FormError } from "./FormError";
+
+type FormTasksProps = {
+  action: (_: string, formData: FormData) => Promise<string>
+}
+
+export const FormTasks: FC<FormTasksProps> = ({action}) => {
+
+  const [task, setTask] = useState('');
+
+  const [errorMessage, formAction, isPending] = useActionState(action,"");
+
+  console.log(errorMessage);
+
+  return (
+    <>
+      {!isPending && <FormError message={errorMessage}/>}
+      
+      <form className ="relative shadow-lg rounded-lg" action={formAction}>
+
+
+        <input className="px-2 py-1 w-full pr-10 border-[#e8e9e9] border hover:border-[#b1b2b2] focus:border-[#b1b2b2] 
+          shadow-md outline-none text-[#7c7c7b] rounded-lg"
+          type="text" name="task" value={task} onChange={(e) => setTask(e.target.value)} 
+          placeholder="Inform the task"
+        />
+
+        <button className=" absolute top-0 right-0 bottom-0 py-1 px-3 rounded-r-lg bg-[#141516] hover:bg-[rgb(86,90,90)] shadow-md cursor-pointer 
+          hover:shadow-none text-white ">+</button>
+
+
+      </form>
+    </>  
+  );
+}
